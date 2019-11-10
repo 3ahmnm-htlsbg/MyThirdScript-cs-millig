bb456db4de4985bb693fc97e3642430b47f73d88
Melissa Illig <melissaillig@Melissas-MacBook-Pro.local>
2019-11-10 12:13:22 +0100

    writing code in VS Code into start method

---
+++ Assets/MyGame/Scripts/Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Numbers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       init a=3;
+       init b=4; 
+
+       Debug.Log("a = " + a);
+
+        Debug.Log("b = " + b);
+
+        Debug.Log("a + b = " + (a + b));
+
+ 
+
+        Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
